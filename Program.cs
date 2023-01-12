0657f087f890daf2818146513122bdb2be47162a
Den <ff12den@gmail.com>
2023-01-13 00:27:08 +0300

    Семинар 1. Четыре задачи

---
+++ Program.cs
@@ -0,0 +1,102 @@
+﻿//                                                                              15.12.2022
+// Первая задача
+
+// Console.Clear();
+// Console.Write("Введите первое число: ");
+// int a = int.Parse(Console.ReadLine()!);
+
+// Console.Write("Введите второе число: ");
+// int b = int.Parse(Console.ReadLine()!);
+
+// if (a > b)
+// {
+//     Console.WriteLine($"{a} есть ваше максимальное число.");
+// }
+// else
+// {
+//        Console.WriteLine($"{b} есть ваше максимальное число.");
+// }
+
+
+
+
+// Вторая задача
+
+// Console.Clear();
+// Console.Write("Введите первое число: ");
+// int a = int.Parse(Console.ReadLine()!);
+
+// Console.Write("Введите второе число: ");
+// int b = int.Parse(Console.ReadLine()!);
+
+// Console.Write("Введите третье число: ");
+// int c = int.Parse(Console.ReadLine()!);
+
+// int max;
+
+// if (a > b)
+// {
+//     if (a > c)
+//     {
+//         max = a;
+//     }
+//     else
+//     {
+//         max = c;
+//     }
+// }
+// else 
+// {
+//     if (b > c)
+//     {
+//         max = b;
+//     }
+//     else
+//     {
+//         max = c;
+//     }
+// }
+
+// Console.WriteLine($"Из вышевведённых чисел максимальным является {max}.");
+
+
+
+
+// Третья задача
+
+// Console.Clear();
+// Console.Write("Введите число: ");
+// int a = int.Parse(Console.ReadLine()!);
+
+// if (a%2 == 0)
+// {
+//     Console.WriteLine($"Это невероятно! Число {a} является чётным!");
+// }
+// else
+// {
+//     Console.WriteLine($"Увы и ах! Число {a} является нечётным...");
+// }
+
+
+
+
+// Четвёртая задача
+
+// Console.Clear();
+// Console.Write("Введите число: ");
+// int a = int.Parse(Console.ReadLine()!);
+
+// int b = 2;
+
+// if (a > 1)
+// {
+//     while (b <= a) {
+//     Console.Write($"{b} ");
+//     b = b + 2;
+//     }
+// }
+// else
+// {
+//     Console.WriteLine($"Введите число побольше.");
+// }
+
